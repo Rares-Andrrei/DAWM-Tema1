4038d23662ebac7a113e7f07180cae8c7fc16bbb
Rares <rares.andrei.tabacaru@gmail.com>
2023-03-27 16:15:42 +0300

    Am adaugat structura aplicatiei, entitatea Recipie si am populat "baza de date" (lista) cu retete

---
+++ WebApp/DataLayer/DbContext.cs
@@ -0,0 +1,57 @@
+﻿using DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public static class DbContext
+    {
+        public static List<Recipie> _recipies = new List<Recipie>
+        {
+            new Recipie
+            {
+                Id = 1,
+                Name = "Donuts",
+                Type = Enums.RecipieType.Breakfast,
+                Ingredients = new List<string>
+                {
+                    "cocoa powder", "milk", "sugar", "flour", "active dry yeast", "salt", "eggs", "oil"
+                }
+            },
+            new Recipie
+            {
+                Id = 2,
+                Name = "Microwave Baked Potato",
+                Type = Enums.RecipieType.Lunch,
+                Ingredients = new List<string>
+                {
+                    "potato", "olive oil", "salt", "butter"
+                }
+            },
+            new Recipie
+            {
+                Id = 3,
+                Name = "Mapo Tofu",
+                Type = Enums.RecipieType.Dinner,
+                Ingredients = new List<string>
+                {
+                    "pound ground pork", "minced fresh ginger", "minced garlic", "cornstarch", "chili crisp", "Pixian chili bean paste", "green onion"
+                }
+            },
+            new Recipie
+            {
+                Id = 4,
+                Name = "Whoopie Pies",
+                Type = Enums.RecipieType.Breakfast,
+                Ingredients = new List<string>
+                {
+                    "flour", "Dutch-process cocoa powder", "baking powder", "baking soda", "kosher salt", "vegetable oil", "unsalted butter",
+                    "packed dark brown sugar", "eggs", "buttermilk"
+                }
+            }
+        };
+    }
+}
---
+++ WebApp/DataLayer/Entities/Recipie.cs
@@ -0,0 +1,35 @@
+﻿using DataLayer.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Entities
+{
+    public class Recipie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public RecipieType Type { get; set; }
+        private List<string> _ingredients;
+
+        public Recipie(int id, string name, RecipieType type, List<string> ingredients)
+        {
+            Id = id;
+            Name = name;
+            _ingredients = ingredients;
+            Type = type;
+        }
+        public Recipie()
+        {
+
+        }
+        public List<string> Ingredients
+        {
+            get { return _ingredients; }
+            set { _ingredients = value; }
+        }
+    }
+}
